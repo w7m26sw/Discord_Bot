4a0a7fe098a9dc90395d33f0fcc98ca1908effae
w7m26sw <w7m26sw@gmail.com>
2021-02-23 02:05:46 +0800

    add cmds Task

--- cmds/main.py
+++ cmds/main.py
@@ -10,7 +10,6 @@ class Main(Cog_Extension):
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f'{round(self.bot.latency*1000)} (ms)')
-        
 
 def setup(bot):
     bot.add_cog(Main(bot))
